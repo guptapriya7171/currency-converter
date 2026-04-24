8fa0567be23549ef4a364a0582be5ead232e242e
Priyanka <guptapriya7171@gmail.com>
2026-04-24 21:46:22 +0530

    my 1st upload from VS after a long

---
+++ app.js
@@ -0,0 +1,67 @@
+const BASE_URL ="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const dropdowns = document.querySelectorAll(".drop-down select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector("form .msg");
+
+window.addEventListener("load", ()=>{
+ updateExchangeRate();
+})
+
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+       // console.log(code, countryList[code]);
+       let newOption = document.createElement("option");
+       newOption.innerText = currCode;
+       newOption.value = currCode;
+
+       if(select.name === "from" && currCode === "USD"){
+        newOption.selected = "selected";
+       }
+       else if(select.name === "to" && currCode === "INR"){
+        newOption.selected = "selected";
+       }
+       select.append(newOption);
+    }
+    select.addEventListener("change", (eve)=>{
+        updateFlag(eve.target);
+    });
+}
+
+const updateFlag =(element)=>{
+     // console.log(element);
+     let currCode = element.value;
+     let countryCode = countryList[currCode];
+     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+     let img = element.parentElement.querySelector("img");
+     img.src = newSrc;
+   
+};
+
+btn.addEventListener("click", (evt)=>{
+    evt.preventDefault();
+    updateExchangeRate();
+})
+
+const updateExchangeRate = async() =>{
+ let amount = document.querySelector(".amount input");
+    let amtValue = amount.value;
+    if(amtValue ==="" || amtValue < 1){
+        amtValue = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  
+    let response = await fetch(URL);
+    let data = await response.json();
+
+    
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    //console.log(rate);
+    let finalAmount = amtValue * rate;
+    msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+
+}
